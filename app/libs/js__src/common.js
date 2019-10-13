a8e427c0375bb368bf7a7728d25d86856fb068aa
mikhail <mikhail2013ru@gmail.com>
2019-10-13 22:35:33 +0300

    js code added

--- app/libs/js__src/common.js
+++ app/libs/js__src/common.js
@@ -5,3 +5,37 @@
 //     scrollTop: $($this.attr('href')).offset().top
 //     }, 500);
 //     });
+//let formContainer = document.getElementById('form-section');
+//let button = document.querySelector('.button');
+
+// button.addEventListener('click', function() {
+//   formContainer.scrollIntoView({behavior: "smooth"});
+//   console.log('К этой кнопке');
+// });
+
+// function scrollToElement(theElement) {
+//   if (typeof theElement === "string") theElement = document.getElementById(theElement);
+//   var selectedPosX = 0;
+//   var selectedPosY = 0;
+   
+//   while (theElement != null) {
+//     selectedPosX += theElement.offsetLeft;
+//     selectedPosY += theElement.offsetTop;
+//     theElement = theElement.offsetParent;
+//   }
+    
+//   window.scrollTo(selectedPosX, selectedPosY);
+// }
+    
+// scrollToElement('#form-container'); // теперь это будет работать
+
+// let button = document.querySelector('.header__button');
+// let footer = document.getElementById('footer');
+
+// console.log(footer);
+
+// button.addEventListener('click', function(event) {
+//   footer.scrollIntoView({
+//       'behavior': 'smooth'
+//   });
+// });
--- app/libs/js__src/navigation.js
+++ app/libs/js__src/navigation.js
@@ -1,22 +1,42 @@
 (function() {
     var me = {};
 
-    me.toggleToActiveLink = function(target) {
-        var links = document.querySelectorAll('.nav__link');
-        var showedSection = target.dataset.link;
+    let button = document.querySelector('a.button');
+    let footer = document.querySelector('.footer');
+    let form = document.querySelector('.form-container');
+    console.log(button);
 
-        for (var i = 0; i < links.length; i++) {
+    button.addEventListener('click', function(e) {
+        e.preventDefault();
+        form.classList.remove('is-hidden');
+        footer.scrollIntoView({
+            'behavior': 'smooth'
+        });
+    });
+
+    this.addEventListener('keydown', function(e) {
+        if (e.keyCode === 27) {
+            form.classList.add('is-hidden');
+        }
+    });
+
+    me.toggleToActiveLink = function(target) {
+        let links = document.querySelectorAll('.nav__link');
+
+        let showedSection = target.dataset.link;
+
+        for (let i = 0; i < links.length; i++) {
             if (links[i].classList.contains('nav__link--active')) {
                 links[i].classList.remove('nav__link--active')
             }            
-        }
+        }       
 
         target.classList.add('nav__link--active');
         scrollToActiveSection(showedSection);
     };
 
     function scrollToActiveSection(showedSection) {
-        var section = document.querySelector('.' + showedSection);
+        let section = document.querySelector('.' + showedSection);
         
         section.scrollIntoView({
             behavior: 'smooth',
